dd56c398665b1a3192b822e456e8452a5876056a
t4r3k <142579274+machine-moon@users.noreply.github.com>
2025-07-18 19:21:33 -0400

    framework api

--- src/cumind/core/__init__.py
+++ src/cumind/core/__init__.py
@@ -1,21 +1,10 @@
 """Core CuMind components."""
 
+from .blocks import ResidualBlock
 from .encoder import BaseEncoder, ConvEncoder, VectorEncoder
 from .mcts import MCTS, Node
 from .mlp import MLPDual, MLPWithEmbedding
 from .network import CuMindNetwork
 from .resnet import ResNet
-from .utils import ResidualBlock
 
-__all__ = [
-    "BaseEncoder",
-    "ConvEncoder",
-    "VectorEncoder",
-    "MCTS",
-    "Node",
-    "MLPDual",
-    "MLPWithEmbedding",
-    "CuMindNetwork",
-    "ResNet",
-    "ResidualBlock",
-]
+__all__ = ["ResidualBlock", "BaseEncoder", "ConvEncoder", "VectorEncoder", "MLPDual", "MLPWithEmbedding", "MCTS", "Node", "CuMindNetwork", "ResNet"]
--- src/cumind/utils/__init__.py
+++ src/cumind/utils/__init__.py
@@ -1,10 +1,10 @@
 """Utility modules for CuMind."""
 
-from .checkpoint import load_checkpoint, save_checkpoint
+from .checkpoint import latest_checkpoints, load_checkpoint, save_checkpoint
 from .config import cfg
-from .jax_utils import batched_apply, safe_normalize, tree_stack, tree_unstack
+from .jax_utils import *  # noqa: F403
 from .logger import log
 from .prng import key
 from .resolve import resolve
 
-__all__ = ["load_checkpoint", "save_checkpoint", "batched_apply", "tree_stack", "tree_unstack", "safe_normalize", "log", "key", "resolve", "cfg"]
+__all__ = ["load_checkpoint", "save_checkpoint", "latest_checkpoints", "cfg", "log", "key", "resolve"]
