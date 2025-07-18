13cd4a1536f1b9f6ce51ad4a28472deccad3122f
t4r3k <142579274+machine-moon@users.noreply.github.com>
2025-07-18 19:22:56 -0400

    no code change here

--- scripts/exportconfig.py
+++ scripts/exportconfig.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!./.venv/bin/python3.12
 from cumind.utils.config import cfg
 
 if __name__ == "__main__":
--- scripts/rmpycache.sh
+++ scripts/rmpycache.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Remove all __pycache__ directories recursively
 find . -type d -name "__pycache__" -exec rm -rf {} + 2>/dev/null || true
