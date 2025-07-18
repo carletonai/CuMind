15d6cf332a30e2d72c245fed9919a346b5398c06
t4r3k <142579274+machine-moon@users.noreply.github.com>
2025-07-18 19:21:33 -0400

    framework api

--- src/cumind/__init__.py
+++ src/cumind/__init__.py
@@ -1,10 +1,11 @@
 """CuMind: A modular reinforcement learning framework."""
 
-from .agent import Agent, Trainer
-from .runner import inference, train
-from .utils import log
-from .utils.config import cfg
-from .utils.prng import key
-
 __version__ = "0.1.7"
-__all__ = ["Agent", "Trainer", "train", "inference", "cfg", "log", "key"]
+
+# Most commonly used components
+from .agent import Agent, inference, train
+from .core import MCTS, CuMindNetwork
+from .data import MemoryBuffer, SelfPlay
+from .utils import cfg, key, log
+
+__all__ = ["Agent", "inference", "train", "CuMindNetwork", "MCTS", "MemoryBuffer", "SelfPlay", "cfg", "log", "key"]
