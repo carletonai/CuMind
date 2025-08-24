bb4202e837fcc8ea48a6a9ec9f79190f041ce9cc
t4r3k <142579274+machine-moon@users.noreply.github.com>
2025-08-24 15:34:09 -0400

    Added signal handling, XLA flag registration, and tracing setup

---
+++ src/cumind/utils/signals.py
@@ -0,0 +1,20 @@
+import signal
+import sys
+
+from cumind.utils.config import cfg
+from cumind.utils.logger import log
+
+
+def register_signals() -> None:
+    def handler(signum: int, frame: object) -> None:
+        log.info(f"Received signal {signum}, shutting down.")
+        log.shutdown()
+        if cfg.tracing.enabled:
+            from jax.profiler import stop_trace
+
+            stop_trace()  #  type: ignore
+
+        sys.exit(1)
+
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
